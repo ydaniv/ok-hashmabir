2df8e94dbab4f7d26e4c5bd61275e7369c4be99b
Yehonatan Daniv <maggotfish@gmail.com>
2012-10-12 19:01:05 +0200

    Added Mustache.js

--- app/app.js
+++ app/app.js
@@ -3,12 +3,13 @@ define([
   "jquery",
   "lodash",
   "backbone",
+  "mustache",
 
   // Plugins.
   "plugins/backbone.layoutmanager"
 ],
 
-function($, _, Backbone) {
+function($, _, Backbone, Mustache) {
 
   // Provide a global location to place configuration settings and module
   // creation.
